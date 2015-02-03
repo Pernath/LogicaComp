dd15db4f0e84d2ad21668d3e7ee27ece1238c381
Pernath <cgah.95@gmail.com>
2015-02-03 16:31:54 -0600

    ya solo restan 6

--- Practicas/practica0/practica0.hs
+++ Practicas/practica0/practica0.hs
@@ -56,14 +56,50 @@ igual Cero (Suc n) = False
 igual (Suc n) Cero = False
 igual (Suc n) (Suc m) = igual n m
 
-power :: Nat -> Nat -> Nat
-power Cero Cero = Suc Cero
-power Cero (Suc n) = Cero
-power (Suc n) Cero = Suc Cero 
-power (Suc n) (Suc Cero) = Suc n
-power (Suc Cero) (Suc n) = Suc Cero
+powerNat :: Nat -> Nat -> Nat
+powerNat Cero Cero = Suc Cero
+powerNat Cero (Suc n) = Cero
+powerNat (Suc n) Cero = Suc Cero 
+powerNat (Suc n) (Suc Cero) = Suc n
+powerNat (Suc Cero) (Suc n) = Suc Cero
 -- a^b = a*[a^(b-1)]
-power (Suc n) (Suc m) = prod (Suc n) (power (Suc n) m)
+powerNat (Suc n) (Suc m) = prod (Suc n) (powerNat (Suc n) m)
+
+--aux 
+esParNat :: Nat -> Bool
+esParNat Cero = True
+esParNat (Suc Cero) = False
+esParNat (Suc (Suc Cero)) = True
+esParNat (Suc (Suc n)) = esParNat n
+
+power :: Int -> Int -> Int
+power 0 0 = 1
+power 0 i = 0
+power i 0 = 1
+power i 1 = i
+power 1 i = 1
+-- a^b = a*a^(b-1)
+power i j = i*(power i (j-1))
+
+esPar :: Int -> Bool
+esPar 0 = True
+esPar 1 = False
+esPar 2 = True
+esPar i = esPar (i-2)
+
+power2 :: Int -> Int -> Int 
+power2 x k
+	| esPar x = power (power x 2) (div k 2) 
+	| otherwise = x*(power x (k-1))
+
+reversa :: [a] -> [a]
+reversa [] = []
+reversa (x:xs) = reversa xs ++ [x]
+
+
+
+
+
 
 
 
